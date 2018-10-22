0b134b7726712b533ab6e56b2bfe67b2dcb6b339
conas1290 <conas1290@7HJL6Y1.Student.UGDSB.ED>
2018-10-22 15:04:06 -0400

    error guards  added  still need one for "" on first input and I think that is it

--- src/customerlist/java/CustomerList.java
+++ src/customerlist/java/CustomerList.java
@@ -10,52 +10,106 @@ import javax.swing.JOptionPane;
 public class CustomerList {
   public static void main(String [] args) throws IOException {
     //Variable setup
-    String userInput,myLine,customerName,customerAddress,customerCity,customerProvince,customerPCode;
+    String userInput,myLine,customerName,customerAddress,customerCity,customerProvince,customerPCode,customerAll,customerAmountStrg;
     int customerAmount;
 
        BufferedReader readFile = new BufferedReader(
        new FileReader("CustomerList.txt"));
       System.out.println("Displaying previous Customers:");
-   
-      //read all the lines of the selected file
-   
-    do{
-      myLine = readFile.readLine();
-      if (myLine != null){
-      System.out.println(myLine);
-      } else{}
-    } while(myLine != null);
-   
-     readFile.close();
-    
+      //try statement
+      try{
+         //read all the lines of the selected file
+        do{
+          myLine = readFile.readLine();
+          if (myLine != null){
+          System.out.println(myLine);
+          } else{}
+         } while(myLine != null);
+         //catch if the file is not found
+        } catch (FileNotFoundException e) {
+                    System.out.println("File does not exist or could not be found.");
+                    System.err.println("FileNotFoundException: " + e.getMessage());
+		} catch (IOException e) {
+                    System.out.println("Problem reading file.");
+                    System.err.println("IOException: " + e.getMessage());
+    	}
+         readFile.close();
+     
+    try{
      PrintWriter fileOut = new PrintWriter(new FileWriter("CustomerList.txt",true));
-     customerAmount =  Integer.parseInt(JOptionPane.showInputDialog("How many customers are there?"));
+     
+     
+     
+   customerAmountStrg = JOptionPane.showInputDialog("How many customers are there?");
+   while(Character.isLetter(customerAmountStrg.charAt(0))||Character.isWhitespace(customerAmountStrg.charAt(0))){
+       JOptionPane.showMessageDialog(null,"Invalid input");
+       customerAmountStrg = JOptionPane.showInputDialog("How many customers are there?");
+   }
+     customerAmount = Integer.parseInt(customerAmountStrg);
      for(int i = 1; i <= customerAmount; i++){
       
-      //Enter users info
+      //Enter Users info
       customerName =  JOptionPane.showInputDialog("Enter Customer Name     ");
       customerAddress =  JOptionPane.showInputDialog("Enter " + customerName + "'s Address");
       customerCity =  JOptionPane.showInputDialog("Enter " + customerName + "'s City");
       customerProvince =  JOptionPane.showInputDialog("Enter " + customerName + "'s Province");
-      customerPCode =  JOptionPane.showInputDialog("Enter " + customerName + "'s Postal Code");
+      customerPCode =  JOptionPane.showInputDialog("Enter " + customerName + "'s Postal Code"+"L#L #L# \n MUST HAVE SPACE");
      
-      //Output directly to file
-      fileOut.println("Name: " + customerName);
-      fileOut.println("Address: " + customerAddress);
-      fileOut.println("City: " + customerCity);
-      fileOut.println("Province: " + customerProvince);
-      fileOut.println("Postal Code: " + customerPCode);
+      while(Character.isDigit(customerPCode.charAt(0))){ 
+          JOptionPane.showMessageDialog(null,"Invalid input");
+          customerPCode =  JOptionPane.showInputDialog("Enter " + customerName + "'s Postal Code"+"L#L #L#");
+      }  
+       while(Character.isLetter(customerPCode.charAt(1))){ 
+         JOptionPane.showMessageDialog(null,"Invalid input");
+         customerPCode =  JOptionPane.showInputDialog("Enter " + customerName + "'s Postal Code"+"L#L #L#");
+      } 
+      while(Character.isDigit(customerPCode.charAt(2))){ 
+         JOptionPane.showMessageDialog(null,"Invalid input");
+         customerPCode =  JOptionPane.showInputDialog("Enter " + customerName + "'s Postal Code"+"L#L #L#");
+      }      
+      while(Character.isWhitespace(customerPCode.charAt(3))){ 
+         JOptionPane.showMessageDialog(null,"Invalid input");
+         customerPCode =  JOptionPane.showInputDialog("Enter " + customerName + "'s Postal Code"+"L#L #L#");
+      }  
+      while(Character.isLetter(customerPCode.charAt(4))){ 
+          JOptionPane.showMessageDialog(null,"Invalid input");
+          customerPCode =  JOptionPane.showInputDialog("Enter " + customerName + "'s Postal Code"+"L#L #L#");
+      }  
+       while(Character.isDigit(customerPCode.charAt(5))){ 
+         JOptionPane.showMessageDialog(null,"Invalid input");
+         customerPCode =  JOptionPane.showInputDialog("Enter " + customerName + "'s Postal Code"+"L#L #L#");
+      } 
+      while(Character.isLetter(customerPCode.charAt(6))){ 
+         JOptionPane.showMessageDialog(null,"Invalid input");
+         customerPCode =  JOptionPane.showInputDialog("Enter " + customerName + "'s Postal Code"+"L#L #L#");
+      }      
+       //make String with all the data
+      customerAll = "Name: "+customerName + "\n" + "Address: "+customerAddress + "\n" 
+      + "City: "+ customerCity + "\n" + "Province: " + customerProvince + 
+      "\n" + "Postal Code: " + customerPCode;
+      
+
+
+
+
+
+      //Output directly to file  
+      fileOut.println(customerAll);   
       fileOut.println(" ");
-     
-      //System print for testing
-      System.out.println("Name: " + customerName);
-      System.out.println("Address: " + customerAddress);
-      System.out.println("City: " + customerCity);
-      System.out.println("Province: " + customerProvince);
-      System.out.println("Postal Code: " + customerPCode);
-      fileOut.println(" ");
+      //Print for user
+      System.out.println(customerAll);
     }
     //The file must be closed after writing is complete
     fileOut.close();
+    }
+    catch(FileNotFoundException e) {
+        System.out.println("File does not exist or could not be found.");
+        System.err.println("FileNotFoundException: " + e.getMessage());
+          }
+    catch (IOException e) {
+       System.out.println("Problem reading file.");
+       System.err.println("IOException: " + e.getMessage());
+   }
+  
   }    
 }
