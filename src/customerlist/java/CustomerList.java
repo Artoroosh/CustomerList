5da10dc004a6ad0b4e1b9afa045045f1f7549524
conas1290 <conas1290@7HJL6Y1.Student.UGDSB.ED>
2018-10-23 14:58:52 -0400

    done

--- src/customerlist/java/CustomerList.java
+++ src/customerlist/java/CustomerList.java
@@ -17,8 +17,8 @@ public class CustomerList {
        new FileReader("CustomerList.txt"));
       System.out.println("Displaying previous Customers:");
       //try statement
-      try{
-         //read all the lines of the selected file
+      try{  
+           //read all the lines of the customer list and 
         do{
           myLine = readFile.readLine();
           if (myLine != null){
@@ -33,21 +33,20 @@ public class CustomerList {
                     System.out.println("Problem reading file.");
                     System.err.println("IOException: " + e.getMessage());
     	}
-         readFile.close();
-     
+         readFile.close(); 
     try{
      PrintWriter fileOut = new PrintWriter(new FileWriter("CustomerList.txt",true));
      
-     
-     
-   customerAmountStrg = JOptionPane.showInputDialog("How many customers are there?");
-   while(Character.isLetter(customerAmountStrg.charAt(0))||Character.isWhitespace(customerAmountStrg.charAt(0))){
+    //ask user to input # of customers and force them to use 
+  
+  customerAmountStrg = JOptionPane.showInputDialog("How many customers are there?");
+   while(customerAmountStrg.length() == 0||!Character.isDigit(customerAmountStrg.charAt(0))){
        JOptionPane.showMessageDialog(null,"Invalid input");
        customerAmountStrg = JOptionPane.showInputDialog("How many customers are there?");
    }
      customerAmount = Integer.parseInt(customerAmountStrg);
      for(int i = 1; i <= customerAmount; i++){
-      
+
       //Enter Users info
       customerName =  JOptionPane.showInputDialog("Enter Customer Name     ");
       customerAddress =  JOptionPane.showInputDialog("Enter " + customerName + "'s Address");
@@ -55,31 +54,31 @@ public class CustomerList {
       customerProvince =  JOptionPane.showInputDialog("Enter " + customerName + "'s Province");
       customerPCode =  JOptionPane.showInputDialog("Enter " + customerName + "'s Postal Code"+"L#L #L# \n MUST HAVE SPACE");
      
-      while(Character.isDigit(customerPCode.charAt(0))){ 
+      while(customerAmountStrg.length() == 0||Character.isDigit(customerPCode.charAt(0))){ 
           JOptionPane.showMessageDialog(null,"Invalid input");
           customerPCode =  JOptionPane.showInputDialog("Enter " + customerName + "'s Postal Code"+"L#L #L#");
       }  
-       while(Character.isLetter(customerPCode.charAt(1))){ 
+       while(customerAmountStrg.length() == 0||Character.isLetter(customerPCode.charAt(1))){ 
          JOptionPane.showMessageDialog(null,"Invalid input");
          customerPCode =  JOptionPane.showInputDialog("Enter " + customerName + "'s Postal Code"+"L#L #L#");
       } 
-      while(Character.isDigit(customerPCode.charAt(2))){ 
+      while(customerAmountStrg.length() == 0||Character.isDigit(customerPCode.charAt(2))){ 
          JOptionPane.showMessageDialog(null,"Invalid input");
          customerPCode =  JOptionPane.showInputDialog("Enter " + customerName + "'s Postal Code"+"L#L #L#");
       }      
-      while(Character.isWhitespace(customerPCode.charAt(3))){ 
+      while(customerAmountStrg.length() == 0||Character.isWhitespace(customerPCode.charAt(3))){ 
          JOptionPane.showMessageDialog(null,"Invalid input");
          customerPCode =  JOptionPane.showInputDialog("Enter " + customerName + "'s Postal Code"+"L#L #L#");
       }  
-      while(Character.isLetter(customerPCode.charAt(4))){ 
+      while(customerAmountStrg.length() == 0||Character.isLetter(customerPCode.charAt(4))){ 
           JOptionPane.showMessageDialog(null,"Invalid input");
           customerPCode =  JOptionPane.showInputDialog("Enter " + customerName + "'s Postal Code"+"L#L #L#");
       }  
-       while(Character.isDigit(customerPCode.charAt(5))){ 
+       while(customerAmountStrg.length() == 0||Character.isDigit(customerPCode.charAt(5))){ 
          JOptionPane.showMessageDialog(null,"Invalid input");
          customerPCode =  JOptionPane.showInputDialog("Enter " + customerName + "'s Postal Code"+"L#L #L#");
       } 
-      while(Character.isLetter(customerPCode.charAt(6))){ 
+      while(customerAmountStrg.length() == 0||Character.isLetter(customerPCode.charAt(6))){ 
          JOptionPane.showMessageDialog(null,"Invalid input");
          customerPCode =  JOptionPane.showInputDialog("Enter " + customerName + "'s Postal Code"+"L#L #L#");
       }      
@@ -88,12 +87,7 @@ public class CustomerList {
       + "City: "+ customerCity + "\n" + "Province: " + customerProvince + 
       "\n" + "Postal Code: " + customerPCode;
       
-
-
-
-
-
-      //Output directly to file  
+  //Output directly to file  
       fileOut.println(customerAll);   
       fileOut.println(" ");
       //Print for user
